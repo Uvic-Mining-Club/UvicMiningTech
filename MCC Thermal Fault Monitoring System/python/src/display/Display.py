87729ce2e94fdf0991b401fbe571b58626c7477a
TDJenks <trevor.jenks9@gmail.com>
2025-11-06 15:42:09 -0800

    Created high-level display functionality

---
+++ MCC Thermal Fault Monitoring System/python/src/display/Display.py
@@ -0,0 +1,13 @@
+from DisplayInterface import DisplayInterface
+
+class Display(DisplayInterface):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def clear(self):
+        pass
+
+    # To be fleshed out in future iterations
